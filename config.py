dce146a374ba5346925247518a721c5d863c32c2
kakarot-6969 <rajsrijan51@gmail.com>
2024-07-25 23:25:32 -0700

    Update config.py

--- config.py
+++ config.py
@@ -22,7 +22,7 @@ DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 180))
 LOGGER_ID = int(getenv("LOGGER_ID", None))
 
 # Get this value from @FallenxBot on Telegram by /id
-OWNER_ID = int(getenv("OWNER_ID", "6203163206"))
+OWNER_ID = int(getenv("OWNER_ID", "6269731241"))
 
 ## Fill these variables if you're deploying on heroku.
 # Your heroku app name
@@ -38,8 +38,8 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/learningbots79")
-SUPPORT_CHAT = getenv("SUPPORT_GROUP", "https://t.me/learning_bots")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "t.me/channelsupport_bo")
+SUPPORT_CHAT = getenv("SUPPORT_GROUP", "https://t.me/channel_work_bots")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
@@ -77,21 +77,21 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://graph.org/file/214f53702f788c668e294.jpg"
+    "START_IMG_URL", "https://telegra.ph/file/396c004bbe73f4b808617.jpg"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://graph.org/file/214f53702f788c668e294.jpg"
+    "PING_IMG_URL", "https://telegra.ph/file/75314f38cc16cb479a9b7.jpg"
 )
-PLAYLIST_IMG_URL = "https://graph.org/file/214f53702f788c668e294.jpg"
-STATS_IMG_URL = "https://graph.org/file/214f53702f788c668e294.jpg"
-TELEGRAM_AUDIO_URL = "https://graph.org/file/214f53702f788c668e294.jpg"
-TELEGRAM_VIDEO_URL = "https://graph.org/file/214f53702f788c668e294.jpg"
-STREAM_IMG_URL = "https://graph.org/file/214f53702f788c668e294.jpg"
-SOUNCLOUD_IMG_URL = "https://graph.org/file/214f53702f788c668e294.jpg"
-YOUTUBE_IMG_URL = "https://graph.org/file/214f53702f788c668e294.jpg"
-SPOTIFY_ARTIST_IMG_URL = "https://graph.org/file/214f53702f788c668e294.jpg"
-SPOTIFY_ALBUM_IMG_URL = "https://graph.org/file/214f53702f788c668e294.jpg"
-SPOTIFY_PLAYLIST_IMG_URL = "https://graph.org/file/214f53702f788c668e294.jpg"
+PLAYLIST_IMG_URL = "https://telegra.ph/file/75314f38cc16cb479a9b7.jpg"
+STATS_IMG_URL = "https://telegra.ph/file/75314f38cc16cb479a9b7.jpg"
+TELEGRAM_AUDIO_URL = "https://telegra.ph/file/75314f38cc16cb479a9b7.jpg"
+TELEGRAM_VIDEO_URL = "https://telegra.ph/file/75314f38cc16cb479a9b7.jpg"
+STREAM_IMG_URL = "https://telegra.ph/file/75314f38cc16cb479a9b7.jpg"
+SOUNCLOUD_IMG_URL = "https://telegra.ph/file/75314f38cc16cb479a9b7.jpg"
+YOUTUBE_IMG_URL = "https://telegra.ph/file/75314f38cc16cb479a9b7.jpg"
+SPOTIFY_ARTIST_IMG_URL = "https://telegra.ph/file/75314f38cc16cb479a9b7.jpg"
+SPOTIFY_ALBUM_IMG_URL = "https://telegra.ph/file/75314f38cc16cb479a9b7.jpg"
+SPOTIFY_PLAYLIST_IMG_URL = "https://telegra.ph/file/75314f38cc16cb479a9b7.jpg"
 
 
 def time_to_seconds(time):
@@ -108,7 +108,7 @@ if SUPPORT_CHANNEL:
             "[ERROR] - Your SUPPORT_CHANNEL url is wrong. Please ensure that it starts with https://"
         )
 
-if SUPPORT_CHAT:
+if SUPPORT_CHAT:https://t.me/channel_work_bots
     if not re.match("(?:http|https)://", SUPPORT_CHAT):
         raise SystemExit(
             "[ERROR] - Your SUPPORT_CHAT url is wrong. Please ensure that it starts with https://"
